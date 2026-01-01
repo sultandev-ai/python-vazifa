3c5efbd72b3d7da8d917fc15fdac699841eae4df
Win <saydulloh.dev2005@gmail.com>
2026-01-01 16:29:43 +0500

    6 chi masala

--- nested loop.py
+++ nested loop.py
@@ -38,6 +38,22 @@
 #     print()
 
 #masala 5
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         if i == 1:
+#             print("#", end=" ")
+#         elif j == 1:
+#             print("#", end=" ")
+#         elif i == 5:
+#             print("#", end=" ")
+#         elif j == 5:
+#             print("#", end=" ")
+#         else:
+#             print("*", end=" ")
+#     print()
+
+#masala 6
+
 for i in range(1, 6):
     for j in range(1, 6):
         if i == 1:
@@ -45,6 +61,8 @@ for i in range(1, 6):
         elif j == 1:
             print("#", end=" ")
         elif i == 5:
+            print("#", end=" ")
+        elif i == 3:
             print("#", end=" ")
         elif j == 5:
             print("#", end=" ")
