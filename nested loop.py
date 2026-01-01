cbed3ef98639190d33eaa015adaaac9f33a81429
Win <saydulloh.dev2005@gmail.com>
2026-01-01 16:14:52 +0500

    2 chi masala

--- nested loop.py
+++ nested loop.py
@@ -1,4 +1,11 @@
-#masala 2
+#masala 1
+# for i in range(1, 6):
+#     for j in range(1,6):
+#         print("*", end=" ")
+#     print()
+
+
+ # masala 2
 for i in range(1, 6):
     print("#", end=" ")
     for j in range(1,6):
