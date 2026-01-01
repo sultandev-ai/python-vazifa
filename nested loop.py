a70866a9db2ab4fbd27e3bc42897f0978db696a3
Win <saydulloh.dev2005@gmail.com>
2026-01-01 16:23:26 +0500

    3 chi masala

--- nested loop.py
+++ nested loop.py
@@ -6,8 +6,19 @@
 
 
  # masala 2
+# for i in range(1, 6):
+#     print("#", end=" ")
+#     for j in range(1,6):
+#         print("*", end=" ")
+#     print()
+
+# masala 3
 for i in range(1, 6):
-    print("#", end=" ")
-    for j in range(1,6):
-        print("*", end=" ")
+    for j in range(1, 6):
+        if i == 1:
+            print("#", end=" ")
+        elif j == 1:
+            print("#", end=" ")
+        else:
+            print("*", end=" ")
     print()
