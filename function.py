7ef8071c997a03930dc0147bcf6bb57d2403c383
sultandev-ai <saydulloh.dev2005@gmail.com>
2026-01-12 19:27:47 +0500

    Add files via upload

---
+++ function.py
@@ -0,0 +1,33 @@
+# 1-masala
+
+# def mod(a):
+#     if a < 0:
+#         return -a
+#     return a
+# print(mod(5))
+# print(mod(-5))
+
+#c2-masala
+
+# def RaqamlikkaTekshir(s):
+#     for ch in s:
+#         if ch >= '0' and ch <= '9':
+#             return True
+#     return False
+#
+# print(RaqamlikkaTekshir("1"))
+# print(RaqamlikkaTekshir("abcd"))
+#
+
+# 3-masala
+
+# def findNumber(s):
+#     natija = ""
+#     for ch in s:
+#         if ch >= '0' and ch <= '9':
+#             natija += ch
+#     return natija
+#
+# print(findNumber("ab12cd45"))
+
+
