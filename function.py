64fecd2f0f4e7c22559865bf5796da7af26fec32
sultandev-ai <saydulloh.dev2005@gmail.com>
2026-01-12 19:57:37 +0500

    Update function.py

--- function.py
+++ function.py
@@ -31,3 +31,77 @@
 # print(findNumber("ab12cd45"))
 
 
+#4-masala
+
+# def KattaHarftopish(s):
+#     for i in s:
+#         if ch >= 'A' and i <= 'Z':
+#             return True
+#     return False
+
+# print(KattaHarftopish("Salom"))
+# print(KattaHarftopish("salom"))
+
+# 5-masala
+
+# def kattaHarflar(s):
+#     natija = ""
+#     for i in s:
+#         if i >= 'A' and ch <= 'Z':
+#             natija += i
+#     return natija
+
+# print(kattaHarf("dsfSiuhfW"))
+
+
+# 6-MASALA
+
+
+# def belgilikkaTekshir(i):
+#     if ('a' <= i <= 'z') or ('A' <= i <= 'Z') or ('0' <= i <= '9'):
+#         return ""
+#     else:
+#         return i
+
+# print(belgilikkaTekshir("@"))
+# print(belgilikkaTekshir("af"))
+
+# 8-masala
+
+# def teskariSatr(s):
+#     i = len(s) - 1
+#     while i >= 0:
+#         print(s[i], end="")
+#         i -= 1
+#     print()
+
+# teskariSatr("Python")
+
+# 9-masala
+
+# def ortaArifmetik(N):
+#     s = 0
+#     i = 0
+#     while i < N:
+#         s += int(input("Son kiriting: "))
+#         i += 1
+#     return s / N
+
+# print(ortaArifmetik(10))
+
+# 10- masala
+# def yoshniHisoblash(hozirgi_yil, tugilgan_yil):
+#     print("Yosh:", hozirgi_yil - tugilgan_yil)
+
+# yoshniHisoblash(2025, 2002)
+
+# 11-masala
+
+# def soatniTopish(N):
+#     print("Soat:", N // 3600)
+
+# soatniTopish(36000)
+
+
+
+
